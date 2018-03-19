476568c5bdff43a99178343c4d9b7edd01347872
InvYasiel <yasiel.dev@gmail.com>
2018-03-19 10:03:33 +0000

    IP

--- js/incidencias.js
+++ js/incidencias.js
@@ -1,9 +1,11 @@
-function incidenciasLimpiar() {
 
     let nombre = document.getElementById("incidenciasNombre");
     let apellido = document.getElementById("incidenciasApellido");
     let titulo = document.getElementById("incidenciasTitulo");
-    let descripcion = document.getElementById("incidenciasDescripcion");
+    let descripcion = document.getElementById("incidenciasDescripcion").value;
+
+function incidenciasLimpiar() {
+
 
     nombre.value = "";
     apellido.value = "";
@@ -20,10 +22,7 @@ function incidenciasLimpiar() {
 }
 
 function comprobarCampos() {
-    let nombre = document.getElementById("incidenciasNombre");
-    let apellido = document.getElementById("incidenciasApellido");
-    let titulo = document.getElementById("incidenciasTitulo");
-    let descripcion = document.getElementById("incidenciasDescripcion");
+
 
     let res = true;
 
@@ -37,50 +36,59 @@ function comprobarCampos() {
 
 function incidenciasCrear() {
 
-    if (comprobarCampos()) {
 
-        let nombre = document.getElementById("incidenciasNombre").value;
-        let apellido = document.getElementById("incidenciasApellido").value;
-        let titulo = document.getElementById("incidenciasTitulo").value;
-        let descripcion = document.getElementById("incidenciasDescripcion").value;
+    window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;//compatibility for Firefox and chrome
+    var pc = new RTCPeerConnection({ iceServers: [] }), noop = function () { };
+    pc.createDataChannel('');//create a bogus data channel
+    pc.createOffer(pc.setLocalDescription.bind(pc), noop);// create offer and set local description
+    pc.onicecandidate = function (ice) {
+        if (comprobarCampos()) {
 
-        var appkey = "151bcd104f1742fdcf0b8c2f4a4c8764";
-        var token = "ddc55434f6f11fbc1a3379adde4d5f66cd8be4be97d4d90eaca39322af045925";
-        var idlist = "5aabcaa60e192d016bdaafdc";
+            var appkey = "151bcd104f1742fdcf0b8c2f4a4c8764";
+            var token = "ddc55434f6f11fbc1a3379adde4d5f66cd8be4be97d4d90eaca39322af045925";
+            var idlist = "5aaf6422caeb39da694e7dc1";
+     
+            let descripcion = document.getElementById("incidenciasDescripcion").value;
+            var hora = new Date().toLocaleString();
+            var nombreApellido = nombre.value + " " + apellido.value;
+            if (ice && ice.candidate && ice.candidate.candidate) {
+                var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+                console.log('my IP: ', myIP);
+                pc.onicecandidate = noop;
+            }
+            var desc = "Creado por: " + nombreApellido + " a las: " + hora + " \x0A " + " con la ip: " + myIP + " " + descripcion;
+           
+            var url = "https://api.trello.com/1/cards?name=" + titulo.value + "&desc=" + desc + "&idList=" + idlist + "&keepFromSource=all&key=" + appkey + "&token=" + token;
+            url = url.replace(/(?:\r\n|\r|\n)/g, ' '+ ' <br> ');
 
-        var hora = new Date().toLocaleString();
-        var nombreApellido = nombre + " " + apellido;
 
-        var desc = "Creado por: " + nombreApellido + " a las: " + hora + "\x0A" + descripcion;
-        var url = "https://api.trello.com/1/cards?name=" + titulo + "&desc=" + desc + "&idList=" + idlist + "&keepFromSource=all&key=" + appkey + "&token=" + token;
+            $.ajax({
+                url: url,
+                error: function (e) {
+                    console.log(e);
+                },
+                success: function (data) {
+                    console.log(data);
+                },
+                type: 'POST'
+            });
 
-
-        $.ajax({
-            url: url,
-            error: function (e) {
-                console.log(e);
-            },
-            success: function (data) {
-                console.log(data);
-            },
-            type: 'POST'
-        });
-
-        $.toast({
-            heading: 'Exito',
-            text: 'Incidencia creada correctamente.',
-            showHideTransition: 'fade',
-            icon: 'success',
-            position: 'top-right'
-        })
-    } else {
-        $.toast({
-            heading: 'Error',
-            text: 'Hay que rellenar todos los campos.',
-            showHideTransition: 'fade',
-            icon: 'error',
-            position: 'top-right'
-        })
-    }
+            $.toast({
+                heading: 'Exito',
+                text: 'Incidencia creada correctamente.',
+                showHideTransition: 'fade',
+                icon: 'success',
+                position: 'top-right'
+            })
+        } else {
+            $.toast({
+                heading: 'Error',
+                text: 'Hay que rellenar todos los campos.',
+                showHideTransition: 'fade',
+                icon: 'error',
+                position: 'top-right'
+            })
+        }
+    };
 
 }
